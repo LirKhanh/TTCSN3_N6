1c5f471ce048dac3294f7f25e8d44d61279d8359
pinkgold1 <162027317+pinkgold1@users.noreply.github.com>
2024-12-07 19:07:58 +0700

    Update MenuAdminController.java

--- src/controllers/admin/MenuAdminController.java
+++ src/controllers/admin/MenuAdminController.java
@@ -2,8 +2,16 @@ package controllers.admin;
 
 import Utils.ConnectJDBCUtil;
 import java.sql.*;
-
-import views.admin.*;
+import views.admin.ManageCategoryUI;
+import views.admin.ManageColorUI;
+import views.admin.ManageCustomerUI;
+import views.admin.ManageDiscountUI;
+import views.admin.ManagePaymentUI;
+import views.admin.ManageSizeUI;
+import views.admin.ManageStaffUI;
+import views.admin.ManageSupplierUI;
+import views.admin.ManageProductUI; // Thêm import cho ManageProductUI
+import views.admin.MenuAdminUI;
 
 public class MenuAdminController {
 
@@ -12,76 +20,70 @@ public class MenuAdminController {
 
     public MenuAdminController(MenuAdminUI view) {
         this.view = view;
-        connection= ConnectJDBCUtil.getConnection();
+        connection = ConnectJDBCUtil.getConnection();
         
         this.view.addManageColorListener(e -> moveManageColor());
         this.view.addManageCustomerListener(e -> moveManageCustomer());
         this.view.addManageSizeListener(e -> moveManageSize());
         this.view.addManageCategoryListener(e -> moveManageCategory());
         this.view.addManageStaffListener(e -> moveManageStaff());
-        
         this.view.addManagePaymentListener(e -> moveManagePayment());
         this.view.addManageDiscountListener(e -> moveManageDiscount());
         this.view.addManageSupplierListener(e -> moveManageSupplier());
-        this.view.addManageHmsListener(e -> moveManageHms());
-        this.view.addManageHmsOutListener(e -> moveManageHmsOut());
-        this.view.addManageReceiptListener(e -> moveManageReceipt());
-
-    }
-    private void moveManageHms() {
-        ManageHMSUI hmsUI = new ManageHMSUI(view);
-        hmsUI.setVisible(true);
-        view.setVisible(false);
+        this.view.addManageProductListener(e -> moveManageProduct()); // Thêm sự kiện cho quản lý 
     }
 
-    private void moveManageHmsOut() {
-        ManageSaleProductUI hmsOutUI = new ManageSaleProductUI(view);
-        hmsOutUI.setVisible(true);
-        view.setVisible(false);
-    }
-    private void moveManageReceipt(){
-        ManageReceiptUI receiptUI = new ManageReceiptUI(view);
-        receiptUI.setVisible(true);
-        view.setVisible(false);
-    }
     private void moveManageColor() {
         ManageColorUI colorUI = new ManageColorUI(view);
         colorUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManageCustomer() {
         ManageCustomerUI customerUI = new ManageCustomerUI(view);
         customerUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManageSize() {
         ManageSizeUI sizeUI = new ManageSizeUI(view);
         sizeUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManageCategory() {
         ManageCategoryUI categoryUI = new ManageCategoryUI(view);
         categoryUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManageStaff() {
         ManageStaffUI staffUI = new ManageStaffUI(view);
         staffUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManagePayment() {
         ManagePaymentUI paymentUI = new ManagePaymentUI(view);
         paymentUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManageDiscount() {
         ManageDiscountUI discountUI = new ManageDiscountUI(view);
         discountUI.setVisible(true);
         view.setVisible(false);
     }
+
     private void moveManageSupplier() {
         ManageSupplierUI supplierUI = new ManageSupplierUI(view);
         supplierUI.setVisible(true);
+        view.setVisible(false);
+    }
+
+    private void moveManageProduct() {
+        ManageProductUI productUI = new ManageProductUI(view);
+        productUI.setVisible(true);
         view.setVisible(false);
     }
 }
